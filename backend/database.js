4e735a53f42babd66eb33aae7dd2507f6fd29485
IT21166792 <lasindusaranga99@gmail.com>
2023-07-15 22:02:03 +0530

    database updated

--- backend/database.js
+++ backend/database.js
@@ -23,7 +23,7 @@ const dbinitialize = async () => {
 }
 
 const readTeachers = async () => {
-    const sql = `SELECT * FROM dummyData`
+    const sql = `SELECT * FROM teacher`
     return new Promise((resolve, reject) => {
         knex_db
             .raw(sql)
--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ if(process.env.NODE_ENV === "test"){
     );
   });
 }else{
-  server.listen(8080, () => {
+  server.listen(8090, () => {
     console.log(
-      "Capstone Project Backend is running on http://localhost:8080"
+      "Capstone Project Backend is running on http://localhost:8090"
     );
   });
 }
